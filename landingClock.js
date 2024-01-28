81a20d814abc253d7809bf21700b69e516dbe2fa
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2024-01-28 14:19:10 +0000

    pud

--- landingClock.js
+++ landingClock.js
@@ -1,12 +1,18 @@
 let timeAngle;
 let width = visualViewport.width;
 let height = visualViewport.height;
+let currentHour;
 function setup() {
   createCanvas(width, height);
+  currentHour = hour();
 }
 
 function draw() {
   timeAngle = calculateTimeAngle();
   background(220);
-  text(timeAngle, width / 2, height / 2);
+  text(
+    `It's Kinda, Maybe about sort of ${currentHour} ish, Maybe`,
+    width / 2,
+    height / 2
+  );
 }
--- timeAngle.js
+++ timeAngle.js
@@ -12,4 +12,3 @@ function calculateTimeAngle() {
   }
   return angle;
 }
-export { calculateTimeAngle };
