d9359a1d3343c958819c628a753accba047fc84a
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2024-01-29 12:59:14 +0000

    now updates by the second for smooth moves

--- orbclock.js
+++ orbclock.js
@@ -60,11 +60,12 @@ function timeCirclehand() {
 function calculateTimeAngle() {
   let currentHour = hour();
   let currentMinute = minute();
+  let currentSecond = second();
   let angle = 0;
   if (currentHour >= 12) {
     currentHour = currentHour - 12;
   }
-  angle = currentHour * 30 + currentMinute * 0.5;
+  angle = currentHour * 30 + currentMinute * 0.5 + currentSecond * (0.5 / 60);
   angle -= 90;
   if (angle < 0) {
     angle += 360;
