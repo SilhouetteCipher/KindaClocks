e8646b5fe2d6ead528af4885b92d55a2b44d2f0b
SilhouetteCipher <141522054+SilhouetteCipher@users.noreply.github.com>
2024-02-05 17:59:00 +0000

    static colour change on trails

---
+++ physicsClockTrail.js
@@ -0,0 +1,173 @@
+let timeAngle;
+let width;
+let height;
+let hourText;
+let minuteText;
+let clockTextSize;
+let particles = [];
+let textGraphics;
+let colorArray = [
+  "#d4b89d",
+  "#3c342d",
+  "#ca4343",
+  "#2455a5",
+  "#2c5b4b",
+  "#ffb82c",
+  "#d5d5d5",
+];
+let lastSwapTime = 0;
+let swapInterval = 180 * 1000; // 60 seconds
+let whiteDuration = 2 * 1000; // 2 seconds
+let particleSmallSize = 10;
+let particleBigSize = 40;
+function setup() {
+  width = windowWidth;
+  height = windowHeight;
+  particleBigSize = width / 42;
+  particleSmallSize = width / 160;
+
+  clockTextSize = width / 4;
+  createCanvas(width, height);
+  textGraphics = createGraphics(width, height);
+  for (let i = 0; i < 1000; i++) {
+    particles[i] = new Particle();
+  }
+}
+
+function draw() {
+  hourText = hour();
+  minuteText = nf(minute(), 2);
+  backgroundTimeText();
+  //image(textGraphics, 0, 0);
+  background(0, 0.1);
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].display();
+    particles[i].checkBlackArea();
+  }
+}
+
+function windowResized() {
+  width = windowWidth;
+  height = windowHeight;
+  clockTextSize = width / 4;
+  particleBigSize = width / 42;
+  particleSmallSize = width / 160;
+  resizeCanvas(windowWidth, windowHeight);
+  textGraphics.resizeCanvas(windowWidth, windowHeight);
+}
+
+function backgroundTimeText() {
+  let currentTime = millis();
+
+  // Check if it's time to make the text white
+  if (currentTime - lastSwapTime > swapInterval) {
+    textGraphics.fill(255);
+    if (currentTime - lastSwapTime > swapInterval + whiteDuration) {
+      textGraphics.fill(0);
+      lastSwapTime = currentTime;
+    }
+  } else {
+    textGraphics.fill(0);
+  }
+
+  textGraphics.background(255);
+  textGraphics.textSize(clockTextSize);
+  textGraphics.textAlign(CENTER, CENTER);
+  textGraphics.textStyle(BOLD);
+
+  textGraphics.textFont("futura");
+  textGraphics.text(hourText + " : " + minuteText, width / 2, height / 2);
+}
+
+class Particle {
+  constructor() {
+    lerpFactor = 0;
+    this.originalColor =
+      colorArray[Math.floor(Math.random() * colorArray.length)];
+    this.color = this.originalColor;
+    this.targetColor = this.originalColor;
+    this.position = createVector(random(width), random(height));
+    this.velocity = createVector(random(-1, 1), random(-1, 1));
+    this.stopped = false;
+    this.stopDelay = 0;
+    this.particleSize = random(particleSmallSize, particleBigSize);
+  }
+
+  update() {
+    if (!this.stopped) {
+      this.position.add(this.velocity);
+      this.checkEdges();
+      this.checkBlackArea();
+      if (this.stopDelay > 0) {
+        this.stopDelay--;
+      } else if (this.stopDelay === 0 && this.isInBlackArea()) {
+        this.velocity = createVector(0, 0);
+        this.stopped = true;
+      }
+    } else {
+      this.checkWhiteArea();
+    }
+  }
+  display() {
+    let pixel = textGraphics.get(this.position.x, this.position.y);
+    let isInBlackArea =
+      red(pixel) === 0 && green(pixel) === 0 && blue(pixel) === 0;
+
+    // Convert originalColor from string to p5.Color
+    let originalColor = color(this.originalColor);
+
+    // Calculate dim color
+    let dimColor = color(
+      red(originalColor) / 4,
+      green(originalColor) / 4,
+      blue(originalColor) / 4
+    );
+
+    // Set lerp factor based on whether the particle is in a black area
+    let lerpFactor = isInBlackArea ? 1 : 0;
+
+    // Use lerpColor to smoothly transition between dimColor and originalColor
+    this.color = lerpColor(dimColor, originalColor, lerpFactor);
+
+    fill(this.color);
+    noStroke();
+    circle(this.position.x, this.position.y, this.particleSize);
+  }
+
+  checkBlackArea() {
+    if (this.isInBlackArea()) {
+      this.stopDelay = floor(random(0, 80)); // delay between 1 to 3 seconds at 60 fps
+    }
+  }
+
+  isInBlackArea() {
+    let pixel = textGraphics.get(this.position.x, this.position.y);
+    return red(pixel) === 0 && green(pixel) === 0 && blue(pixel) === 0;
+  }
+
+  checkWhiteArea() {
+    let pixel = textGraphics.get(this.position.x, this.position.y);
+    if (red(pixel) > 0 || green(pixel) > 0 || blue(pixel) > 0) {
+      this.velocity = createVector(random(-1, 1), random(-1, 1));
+      this.stopped = false;
+    }
+  }
+
+  checkEdges() {
+    if (this.position.x >= width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width - 1;
+    }
+    if (this.position.y >= height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.y = height - 1;
+    }
+  }
+}
+
+function resetCanvas() {
+  textGraphics.clear();
+}
